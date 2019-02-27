9be95cb2dac70b8626f8a776dcb676f0e15dfa65
allan <gjbae1212@gmail.com>
2019-02-27 16:07:46 +0900

    Add time util.

---
+++ util/time.go
@@ -0,0 +1,50 @@
+package util
+
+import "time"
+
+var (
+	maxTime      = time.Unix(0, (1<<63)-1)
+	utcLayout    = "2006-01-02 15:04:05"
+	dailyLayout  = "20060102"
+	hourlyLayout = "2006010215"
+)
+
+func TimestampByMaxTime() int64 {
+	return maxTime.UnixNano()
+}
+
+func StringToTime(s string) time.Time {
+	t, err := time.Parse(utcLayout, s)
+	if err != nil {
+		return time.Time{}
+	}
+	return t
+}
+
+func TimeToString(t time.Time) string {
+	return t.Format(utcLayout)
+}
+
+func DailyStringToTime(s string) time.Time {
+	t, err := time.Parse(dailyLayout, s)
+	if err != nil {
+		return time.Time{}
+	}
+	return t
+}
+
+func TimeToDailyStringFormat(t time.Time) string {
+	return t.Format(dailyLayout)
+}
+
+func HourlyStringToTime(s string) time.Time {
+	t, err := time.Parse(hourlyLayout, s)
+	if err != nil {
+		return time.Time{}
+	}
+	return t
+}
+
+func TimeToHourlyStringFormat(t time.Time) string {
+	return t.Format(hourlyLayout)
+}
